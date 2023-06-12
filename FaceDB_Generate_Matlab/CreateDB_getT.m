8d777ec5fdf330f559095aebd3c773051abf2167
LokiXun <2682414501@qq.com>
2023-06-12 12:28:42 +0800

    fix:image_dir_database dir type check

--- FaceDB_Generate_Matlab/CreateDB_getT.m
+++ FaceDB_Generate_Matlab/CreateDB_getT.m
@@ -7,33 +7,52 @@ function [Train_struct,Test_struct,T]=CreateDB_getT(DB_path,change_size)
 % 1.将训练、测试图像的路径字符串存入结构体
 image_dirname=dir(DB_path);% 每个人的人脸图像存储于单独一个文件夹内
 Train_index=1;Test_index=1;
+filetype_simple_checklist = {'.tar', '.zip'};
 for i=1:size(image_dirname,1)
-    if not(strcmp(image_dirname(i).name,'.')|strcmp(image_dirname(i).name,'..'))
-        person_path=strcat(DB_path,'\',image_dirname(i).name);%人名s1，s2的路径
-        personal_dirname=dir(person_path);%该人的人脸图文件名struct
-        
-        face_num=0;%数此人的人脸图像个数
-        for j=1:size(personal_dirname,1)
-            if not(strcmp(personal_dirname(j).name,'.')|strcmp(personal_dirname(j).name,'..'))
-                face_num=face_num+1;
-            end
+    image_dir_name = image_dirname(i).name;
+    
+    % judge whether to skip this dir
+    skip_this_dir_flag = false;
+    for tmp=1:length(filetype_simple_checklist)
+        if endsWith(image_dir_name, filetype_simple_checklist{tmp})
+            skip_this_dir_flag = true;
+            break;
         end
-        
-        train_num=floor(face_num*0.9);%取80%图像作训练
-        test_num=face_num-train_num;
-        for j=1:size(personal_dirname,1)
-            if not(strcmp(personal_dirname(j).name,'.')|strcmp(personal_dirname(j).name,'..'))
-                if train_num>0 %训练图像（前80%张）
-                    Train_struct(Train_index).person_name=image_dirname(i).name;
-                    Train_struct(Train_index).path=strcat(person_path,'\',personal_dirname(j).name);
-                    Train_index=Train_index+1;
-                    train_num=train_num-1;
-                elseif test_num>0
-                    Test_struct(Test_index).person_name=image_dirname(i).name;
-                    Test_struct(Test_index).path=strcat(person_path,'\',personal_dirname(j).name);
-                    Test_index=Test_index+1;
-                    test_num=test_num-1;
-                end
+    end
+
+    if strcmp(image_dirname(i).name,'.')||strcmp(image_dirname(i).name,'..')
+        skip_this_dir_flag = true;
+    end
+
+    if skip_this_dir_flag
+        continue;
+    end
+
+
+    person_path=strcat(DB_path,'\',image_dirname(i).name);%人名s1，s2的路径
+    personal_dirname=dir(person_path);%该人的人脸图文件名struct
+    
+    face_num=0;%数此人的人脸图像个数
+    for j=1:size(personal_dirname,1)
+        if not(strcmp(personal_dirname(j).name,'.')|strcmp(personal_dirname(j).name,'..'))
+            face_num=face_num+1;
+        end
+    end
+    
+    train_num=floor(face_num*0.9); % take parts as training set
+    test_num=face_num-train_num;
+    for j=1:size(personal_dirname,1)
+        if not(strcmp(personal_dirname(j).name,'.')|strcmp(personal_dirname(j).name,'..'))
+            if train_num>0 %训练图像（前80%张）
+                Train_struct(Train_index).person_name=image_dirname(i).name;
+                Train_struct(Train_index).path=strcat(person_path,'\',personal_dirname(j).name);
+                Train_index=Train_index+1;
+                train_num=train_num-1;
+            elseif test_num>0
+                Test_struct(Test_index).person_name=image_dirname(i).name;
+                Test_struct(Test_index).path=strcat(person_path,'\',personal_dirname(j).name);
+                Test_index=Test_index+1;
+                test_num=test_num-1;
             end
         end
     end
--- README.md
+++ README.md
@@ -52,5 +52,37 @@ author LokiXun  2682414501@qq.com
    - could not detect the face region :fire:
      库文件运行异常》运行安装目录下的例程 `E:\Labview2017\examples\Vision\NIVision OpenCV Utilities\OpenCV Face Detection.vi`
    
-   
 
+
+
+
+
+## Details
+
+- PCA feature extraction
+
+  1. Transform to gray scale image
+  2. resize to [200, 200] & reshape to [200x200, 1] put as columns vectors into `matrix T`
+  3. compute mean column vector in `matrix T` & each vectors  minus mean vectors to get centric matrix `A`
+
+  
+
+- `rgb2gray`
+
+  Change from RGB space to `YUV` or `YCbCr` color space and use Y value as gray-scale image.
+
+  `rgb2gray` 通过计算 R、G 和 B 分量的加权和，将 RGB 值转换为灰度值: $0.2989 * R + 0.5870 * G + 0.1140 * B $
+
+  - YUV space 
+
+    mainly consider the simplicity to transform the color space, which usually used be compatible  Black&White TV.
+    $$
+    Y = 0.2989 * R + 0.5870 * G + 0.1140 * B \\
+    U = B - Y \\
+    V = R - Y \\
+    where~Y~is~brightness,~U,V~is~color~difference.
+    $$
+
+   Similarly, Y value in `YCbCr` Space is also computed by this equation, denotes the brightness.
+
+  
